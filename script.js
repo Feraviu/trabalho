b1ab38c15a648f524bcdbdd902b829267571e465
Feraviu <yuriblf128@gmail.com>
2024-11-01 09:41:13 -0300

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('contatoForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Mensagem enviada! Entraremos em contato em breve.');
-    this.reset();
-});
